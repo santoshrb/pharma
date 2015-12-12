78fa1542ee52a51b7853ae5e5b0a7f8ffb82de19
Santosh Kumbhar <kumbharsansk@gmail.com>
2015-12-12 19:15:07 +0530

    fil: UI design

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -23,4 +23,10 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
   	assert_select "title", "About | #{@base_title}"
   end
+
+  test "should get about" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
+  end
 end
